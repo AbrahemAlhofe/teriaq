8c21ddd87eff4b51a548ac62efee7c17855d0c26
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2024-03-30 20:34:47 +0200

    chore: add content

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const $contactUs__thumbnail = document.querySelector("#contact-us .thumbnail");
 for ( let i = 0; i <= 15; i += 1 ) {
     const $bar = document.createElement("div");
     $bar.classList.add("bar");
-    $bar.innerText = "إتصل بنا ".repeat(20);
+    $bar.innerText = "Contact Us ".repeat(30);
     $contactUs__thumbnail.appendChild($bar);
 }
